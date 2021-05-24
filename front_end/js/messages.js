29493f4fd612d8056be6a33b09fbabf06c2db0ac
CHANTHY-THA <chanthy.tha@student.passerellesnumeriques.org>
2021-05-24 15:34:05 +0700

    Add function remove

--- front_end/js/messages.js
+++ front_end/js/messages.js
@@ -5,16 +5,24 @@ const GET_MESSAGE_REQUEST = "http://" + IP + ":" + PORT + "/message";
 
 function showMessage(response){
   let isText = response.data;
-  let message = document.querySelector(".message");
+  let contence = document.querySelector(".contence");
+  let para = document.querySelector(".message");
  
+  if (para !== null){
+    para.remove();
+  }
+  let message = document.createElement("div");
+  message.className = "message";
   for (let use of isText){
     if (use.text !== ""){
       let child = document.createElement('div');
       child.className = "paragraph"
       let span = document.createElement("span");
       span.textContent = use.text;
+      span.style.background = "gray"
       child.appendChild(span);
       message.appendChild(child);
+      contence.appendChild(message)
     }
   }
   text.value = "";
@@ -23,20 +31,20 @@ function showMessage(response){
 function setMessage(){
   if (text.value !== ""){
     let word = {text: text.value};
-    console.log(word)
-    axios.post(GET_MESSAGE_REQUEST, word).then(showMessage);
+    axios.post(GET_MESSAGE_REQUEST, word).then(getMessage);
   }
 }
 
 function getMessage() {
-
-  axios.get(GET_MESSAGE_REQUEST).then(setMessage);
-
+  axios.get(GET_MESSAGE_REQUEST).then(showMessage);
 } 
 
+let p = document.querySelector("p")
+let item = JSON.parse(localStorage.getItem("user"));
+p.textContent = item.user;
+
 let text = document.querySelector("#text");
-
 let send = document.querySelector(".submit");
-send.addEventListener("click", getMessage);
+send.addEventListener("click", setMessage);
 
-// setInterval(getMessage, 1000);
+// setInterval(getMessage, 3000);
